a2fb1bb31b7d4eb9be3c94c77889dcfbca6a17e5
Matthew Huie <mphuie@gmail.com>
2013-02-04 21:20:15 -0800

    Added sqlalchemy imports

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,4 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 from myapp.views import aview
+from myapp.database import db_session
 
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
